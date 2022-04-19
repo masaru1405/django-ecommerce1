ae9843a54a1e814abef8c9762b7032fbc0278628
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-04-19 18:19:27 -0300

    initial commit, add cards

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'myapp'
+urlpatterns = [
+   path('', views.index, name='index'),
+   path('products/', views.products, name='products'),
+   path('products/<int:id>/', views.product_detail, name='product_detail')
+]
---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+def index(request):
+   return HttpResponse("Hello World!")
+
+def products(request):
+   products = Product.objects.all()
+   context = {'products': products}
+   return render(request, 'myapp/index.html', context)
+
+def product_detail(request, id):
+   product = Product.objects.get(id=id)
+   context = {'product': product}
+   return render(request, 'myapp/detail.html', context)
