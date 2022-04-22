c5aab3ec588e805fa356e897a9af0af2fa14b3b6
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-04-22 09:13:17 -0300

    add user

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,8 @@ app_name = 'myapp'
 urlpatterns = [
    path('', views.index, name='index'),
    path('products/', views.products, name='products'),
-   path('products/<int:id>/', views.product_detail, name='product_detail')
+   path('products/<int:id>/', views.product_detail, name='product_detail'),
+   path('products/add/', views.product_add, name='product_add'),
+   path('products/update/<int:id>/', views.product_update, name='product_update'),
+   path('products/delete/<int:id>/', views.product_delete, name='product_delete')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -14,3 +14,39 @@ def product_detail(request, id):
    product = Product.objects.get(id=id)
    context = {'product': product}
    return render(request, 'myapp/detail.html', context)
+
+def product_add(request):
+   if request.method == 'POST':
+      name = request.POST.get('name')
+      price = request.POST.get('price')
+      desc = request.POST.get('desc')
+      image = request.FILES['upload']
+      product = Product(name=name, price=price, desc=desc, images=image)
+      product.save()
+      return redirect('/myapp/products')
+   return render(request, 'myapp/addproduct.html')
+
+
+def product_update(request, id):
+   product = Product.objects.get(id=id)
+   if request.method == 'POST':
+      product.name = request.POST.get('name')
+      product.price = request.POST.get('price')
+      product.desc = request.POST.get('desc')
+      product.images = request.FILES['upload'] #dá erro se não der upload de uma imagem
+      product.save()
+      return redirect('/myapp/products') #é a url e não o arquivo html
+   context = {
+      'product': product
+   }
+   return render(request, 'myapp/updateproduct.html', context)
+
+def product_delete(request, id):
+   product = Product.objects.get(id=id)
+   context = {
+      'product': product
+   }
+   if request.method == 'POST':
+      product.delete()
+      return redirect('/myapp/products')
+   return render(request, 'myapp/delete.html', context)
